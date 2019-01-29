a4aad906fe0355ed9311509df2dfb393988c0314
E Jun <jason_e@syscom.com.cn>
2019-01-29 17:13:50 +0800

    can sign in

--- app/src/main/java/com/thumbstage/hydrogen/view/sign/SignInFragment.java
+++ app/src/main/java/com/thumbstage/hydrogen/view/sign/SignInFragment.java
@@ -7,13 +7,27 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.EditText;
+import android.widget.Toast;
 
+import com.avos.avoscloud.AVException;
+import com.avos.avoscloud.AVUser;
+import com.avos.avoscloud.LogInCallback;
 import com.thumbstage.hydrogen.R;
+import com.thumbstage.hydrogen.view.common.SignEvent;
 
+import org.greenrobot.eventbus.EventBus;
+
+import butterknife.BindView;
 import butterknife.ButterKnife;
 import butterknife.OnClick;
 
 public class SignInFragment extends Fragment {
+
+    @BindView(R.id.fragment_signIn_name)
+    EditText name;
+    @BindView(R.id.fragment_signIn_password)
+    EditText password;
 
     @Nullable
     @Override
@@ -26,6 +40,29 @@ public class SignInFragment extends Fragment {
     @OnClick(R.id.fragment_signIn_signIn)
     public void signIn(View view) {
 
+        if( name.getText().toString().isEmpty() ) {
+            Toast toast = Toast.makeText(getContext(), getContext().getString(R.string.attention_empty_name), Toast.LENGTH_SHORT);
+            toast.show();
+            return;
+        } else if( password.getText().toString().isEmpty() ) {
+            Toast toast = Toast.makeText(getContext(), getContext().getString(R.string.attention_empty_password), Toast.LENGTH_SHORT);
+            toast.show();
+            return;
+        }
+
+        AVUser.logInInBackground(name.getText().toString(), password.getText().toString(), new LogInCallback<AVUser>() {
+            @Override
+            public void done(AVUser user, AVException e) {
+                if( e == null ) {
+                    SignEvent event = new SignEvent(user, "signUser");
+                    EventBus.getDefault().post(event);
+                    ((SignActivity) getActivity()).onSupportNavigateUp();
+                } else {
+                    Toast toast = Toast.makeText(getContext(), getContext().getString(R.string.wrong_name_password), Toast.LENGTH_SHORT);
+                    toast.show();
+                }
+            }
+        });
     }
 
     @OnClick(R.id.fragment_signIn_signUp)
--- app/src/main/java/com/thumbstage/hydrogen/view/sign/SignUpFragment.java
+++ app/src/main/java/com/thumbstage/hydrogen/view/sign/SignUpFragment.java
@@ -1,6 +1,5 @@
 package com.thumbstage.hydrogen.view.sign;
 
-import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
