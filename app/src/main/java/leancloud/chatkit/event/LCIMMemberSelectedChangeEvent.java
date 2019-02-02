3a669f228df4572be92a3037f2f92c9176b731be
E Jun <jason_e@syscom.com.cn>
2019-02-02 10:26:55 +0800

    mark for port leancloud chatkit

---
+++ app/src/main/java/leancloud/chatkit/event/LCIMMemberSelectedChangeEvent.java
@@ -0,0 +1,8 @@
+package leancloud.chatkit.event;
+
+import leancloud.chatkit.LCChatKitUser;
+
+public class LCIMMemberSelectedChangeEvent {
+    public LCChatKitUser member;
+    public boolean isSelected;
+}
