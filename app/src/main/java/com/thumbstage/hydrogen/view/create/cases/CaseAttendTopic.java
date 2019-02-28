402240a8382d27d963461f80fc9f7b043f771da3
E Jun <jason_e@syscom.com.cn>
2019-02-28 16:34:44 +0800

    CaseAttendTopic can sendLine

--- app/src/main/java/com/thumbstage/hydrogen/view/create/cases/CaseAttendTopic.java
+++ app/src/main/java/com/thumbstage/hydrogen/view/create/cases/CaseAttendTopic.java
@@ -1,34 +1,27 @@
 package com.thumbstage.hydrogen.view.create.cases;
 
+import android.text.TextUtils;
+
 import com.thumbstage.hydrogen.data.LCRepository;
 import com.thumbstage.hydrogen.model.Line;
-import com.thumbstage.hydrogen.model.Topic;
+import com.thumbstage.hydrogen.model.Pipe;
 import com.thumbstage.hydrogen.event.ConversationBottomBarEvent;
 
 public class CaseAttendTopic extends CaseBase {
 
     @Override
     public void handleBottomBarEvent(final ConversationBottomBarEvent event) {
-        if(pipe == null) {
-            LCRepository.copyPublishedOpenedTopic(topic, new LCRepository.ICallBack() {
+        if(TextUtils.isEmpty(pipe.getId())) {
+            LCRepository.copyPublishedOpenedTopic(topic, new LCRepository.IReturnPipe() {
                 @Override
-                public void callback(String objectID) {
-                    pipe.setId(objectID);
-                    // AVIMConversation conv = UserGlobal.getInstance().getConversation(objectID);
-                    // imConversation = conv;
+                public void callback(Pipe p) {
+                    pipe = p;
                     handleEvent(event);
                 }
             });
         } else {
             handleEvent(event);
         }
-    }
-
-    @Override
-    public CaseBase setTopic(Topic topic) {
-        this.topic = topic;
-        pipe = null;
-        return this;
     }
 
     protected void handleEvent(ConversationBottomBarEvent event) {
--- app/src/main/java/com/thumbstage/hydrogen/view/create/cases/CaseBase.java
+++ app/src/main/java/com/thumbstage/hydrogen/view/create/cases/CaseBase.java
@@ -1,7 +1,9 @@
 package com.thumbstage.hydrogen.view.create.cases;
 
 import android.support.v7.widget.LinearLayoutManager;
+import android.util.Log;
 
+import com.thumbstage.hydrogen.data.LCRepository;
 import com.thumbstage.hydrogen.model.Line;
 import com.thumbstage.hydrogen.model.Pipe;
 import com.thumbstage.hydrogen.model.Topic;
@@ -25,6 +27,7 @@ public abstract class CaseBase implements ITopicFragmentFunction {
 
     public CaseBase setTopic(Topic topic) {
         this.topic = topic;
+        addLines2Adapter(topic.getDialogue());
         return this;
     }
 
@@ -44,98 +47,29 @@ public abstract class CaseBase implements ITopicFragmentFunction {
     }
 
     protected void addLine2Pipe(Line line) {
-
+        LCRepository.sendLine(pipe, line, new LCRepository.IReturnBool() {
+            @Override
+            public void callback(Boolean isOK) {
+                if(isOK)  {
+                    Log.i(TAG, "addLine2Pipe ok");
+                } else {
+                    // TODO: 2/28/2019 diagnose a fault
+                }
+            }
+        });
     }
 
     protected void addLine2Adapter(Line line) {
         topicAdapter.addItems(line);
-        addLine2Topic(line);
+        scrollToBottom();
     }
 
     protected void addLines2Adapter(List<Line> lines) {
-
+        topicAdapter.setItems(lines);
     }
-
-    private void addLine2Topic(Line line) {
-        topic.getDialogue().add(line);
-    }
-
-    /*
-    protected void sendText(String content) {
-        AVIMTextMessage message = new AVIMTextMessage();
-        message.setText(content);
-        Map<String, Object> attributes = new HashMap<String, Object>();
-        attributes.put("type", LineType.LT_DIALOGUE.name());
-        message.setAttrs(attributes);
-        sendMessage(message);
-    }
-
-    protected void sendMessage(AVIMMessage message) {
-        sendMessage(message, true);
-    }
-
-    protected void addToList(AVIMMessage message) {
-        topicAdapter.addMessage(message);
-    }
-
-    protected void sendMessage(final AVIMMessage message, boolean addToList) {
-        if (addToList) {
-            topicAdapter.addMessage(message);
-        }
-        topicAdapter.notifyDataSetChanged();
-        scrollToBottom();
-        if( pipe != null) {
-            AVIMMessageOption option = new AVIMMessageOption();
-            option.setReceipt(true);
-            final AVIMConversation imConversation = UserGlobal.getInstance().getConversation(pipe.getId());
-            imConversation.sendMessage(message, option, new AVIMConversationCallback() {
-                @Override
-                public void done(AVIMException e) {
-                    topicAdapter.notifyDataSetChanged();
-                    if (null != e) {
-                        LogUtils.logException(e);
-                    } else {
-                        LCRepository.addTopicOneLine(pipe,
-                                DataConvertUtil.convert2Line(message), new LCRepository.ICallBack() {
-                                    @Override
-                                    public void callback(String objectID) {
-                                        Log.i(TAG, "addTopicOneLine ok");
-                                    }
-                                });
-                        // TODO: 2/22/2019 must to synchronous loca
-                        // topic.getDialogue().add(new Line()); // local topic add line
-                        // EventBus.getDefault().post(new );
-                    }
-                }
-            });
-
-        }
-    }
-    */
 
     protected void scrollToBottom() {
         layoutManager.scrollToPositionWithOffset(topicAdapter.getItemCount() - 1, 0);
     }
 
-    /*
-    protected void appendTopicDialogue(Topic topic) {
-        for(Line line : topic.getDialogue()) {
-            AVIMTypedMessage m;
-            if( StringUtil.isUrl(line.getWhat()) ) { // default is Audio
-                AVFile file = new AVFile("music", line.getWhat(), null);
-                m = new AVIMAudioMessage(file);
-            } else { // is text
-                m = new AVIMTextMessage();
-                ((AVIMTextMessage)m).setText(line.getWhat());
-            }
-            m.setFrom(line.getWho());
-            m.setTimestamp(line.getWhen().getTime());
-            addToList(m);
-        }
-    }
-
-    public void clearTopic() {
-        topic = new Topic();
-    }
-    */
 }
