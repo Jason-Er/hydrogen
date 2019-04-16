d26b158e3e9d8d71ddcc6237120d07cab60d7e9e
E Jun <jason_e@syscom.com.cn>
2019-04-16 09:27:11 +0800

    add WaveSideBarView

--- app/src/main/java/com/thumbstage/hydrogen/view/show/fragment/ShowFragment.java
+++ app/src/main/java/com/thumbstage/hydrogen/view/show/fragment/ShowFragment.java
@@ -10,7 +10,6 @@ import android.speech.tts.UtteranceProgressListener;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
-import android.support.v7.widget.LinearLayoutManager;
 import android.text.TextUtils;
 import android.util.Log;
 import android.view.Gravity;
@@ -58,7 +57,6 @@ public class ShowFragment extends Fragment implements TextToSpeech.OnInitListene
     ViewModelProvider.Factory viewModelFactory;
     TopicViewModel topicViewModel;
 
-    LinearLayoutManager layoutManager;
     TextToSpeech textToSpeech;
 
     @Nullable
