2eacf2e3c21e56cda3e000f244dfb2ed41fb5f0f
E Jun <jason_e@syscom.com.cn>
2019-06-06 15:59:41 +0800

    locate participant circularly

--- app/src/main/java/com/thumbstage/hydrogen/view/show/fragment/ShowLayoutManager.java
+++ app/src/main/java/com/thumbstage/hydrogen/view/show/fragment/ShowLayoutManager.java
@@ -1,5 +1,6 @@
 package com.thumbstage.hydrogen.view.show.fragment;
 
+import android.graphics.Point;
 import android.graphics.Rect;
 import android.support.v7.widget.RecyclerView;
 import android.view.View;
@@ -7,6 +8,7 @@ import android.view.View;
 public class ShowLayoutManager extends RecyclerView.LayoutManager {
 
     int decoratedChildWidth, decoratedChildHeight;
+    Point center;
 
     @Override
     public RecyclerView.LayoutParams generateDefaultLayoutParams() {
@@ -26,61 +28,67 @@ public class ShowLayoutManager extends RecyclerView.LayoutManager {
         }
         detachAndScrapAttachedViews(recycler);
 
+        center = new Point(getHorizontalSpace()/2,getVerticalSpace()/2);
+
         View firstView = recycler.getViewForPosition(0);
         addView(firstView);
         measureChildWithMargins(firstView, 0, 0);
         decoratedChildWidth = getDecoratedMeasuredWidth(firstView);
         decoratedChildHeight = getDecoratedMeasuredHeight(firstView);
+
         switch (getItemCount()) {
             case 1:
-                layoutOneParticipant(recycler);
+                layoutParticipantCircle(recycler, 0, 0);
                 break;
             case 2:
-                layoutTwoParticipant(recycler);
+                layoutParticipantCircle(recycler, 0, getHorizontalSpace()/6);
                 break;
             case 3:
+                layoutParticipantCircle(recycler, -90, getVerticalSpace()/2 - decoratedChildHeight);
                 break;
-            case 4:
+            default:
+                layoutParticipantCircle(recycler, -90, getVerticalSpace()/2 - decoratedChildHeight);
                 break;
         }
     }
 
-    private void layoutOneParticipant(RecyclerView.Recycler recycler) {
+    private Point produceCirclePoint(Point center, double angle, int currentRadius) {
+        Point point = new Point();
+        float radian = (float) Math.toRadians(angle);
+        int x = center.x + (int) (Math.cos(radian) * currentRadius);
+        int y = center.y + (int) (Math.sin(radian) * currentRadius);
+        point.set(x,y);
+        return point;
+    }
+
+    private Rect produceLayoutRect(Point center, int rectWidth, int rectHeight) {
+        Rect rect = new Rect(
+                center.x - rectWidth/2,
+                center.y - rectHeight/2,
+                center.x + rectWidth/2,
+                center.y + rectHeight/2
+        );
+        return rect;
+    }
+
+    private void layoutParticipantCircle(RecyclerView.Recycler recycler, double initialAngle, int radius) {
         View view = getChildAt(0);
-        Rect center = new Rect(
-                (getHorizontalSpace() - decoratedChildWidth)/2,
-                (getVerticalSpace() - decoratedChildHeight)/2,
-                (getHorizontalSpace() + decoratedChildWidth)/2,
-                (getVerticalSpace() + decoratedChildHeight)/2
-        );
-        layoutDecorated(view, center.left, center.top, center.right, center.bottom);
+        Point point = produceCirclePoint(center, initialAngle, radius);
+        Rect rect = produceLayoutRect(point, decoratedChildWidth, decoratedChildHeight);
+        layoutDecorated(view, rect.left, rect.top, rect.right, rect.bottom);
+        double angle = initialAngle;
+        double intervalAngle = 360 / getItemCount();
+        for(int i=1; i<getItemCount(); i++) {
+            angle += intervalAngle;
 
-    }
-    private void layoutTwoParticipant(RecyclerView.Recycler recycler) {
-        View view = getChildAt(0);
-        Rect center = new Rect(
-                getHorizontalSpace()/3 - decoratedChildWidth/2,
-                (getVerticalSpace() - decoratedChildHeight)/2,
-                getHorizontalSpace()/3 + decoratedChildWidth/2,
-                (getVerticalSpace() + decoratedChildHeight)/2
-        );
-        layoutDecorated(view, center.left, center.top, center.right, center.bottom);
-        View view2 = recycler.getViewForPosition(1);
-        addView(view2);
-        measureChildWithMargins(view2, 0, 0);
-        Rect center2 = new Rect(
-                getHorizontalSpace()*2/3 - decoratedChildWidth/2,
-                (getVerticalSpace() - decoratedChildHeight)/2,
-                getHorizontalSpace()*2/3 + decoratedChildWidth/2,
-                (getVerticalSpace() + decoratedChildHeight)/2
-        );
-        layoutDecorated(view2, center2.left, center2.top, center2.right, center2.bottom);
-    }
-    private void layoutThreeParticipant(RecyclerView.Recycler recycler) {
+            view = recycler.getViewForPosition(i);
+            addView(view);
+            measureChildWithMargins(view, 0, 0);
 
-    }
-    private void layoutFourParticipant(RecyclerView.Recycler recycler) {
-
+            point = produceCirclePoint(center, angle, radius);
+            rect = produceLayoutRect(point, decoratedChildWidth, decoratedChildHeight);
+            layoutDecorated(view, rect.left, rect.top, rect.right, rect.bottom);
+        }
     }
 
     private int getVerticalSpace() {
